467797314add11351e4d42e5eeb1397c8f804bb0
siwei <siwei@siweis-Mac-Studio.local>
2024-07-16 10:39:05 +0800

    minor for main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
-lean="positive"
+lean="positive" # 正面贡献、负面贡献
 temperature=1
-focus="self"
-task="percent"
-iteration=2
+focus="self" # 只估计自己、先估计别人再估计自己
+task="percent" # percent or rank
 model="gpt-4o"
+gender="None" # male, female, None
+
+iteration=2 #
 
 os.system("python3 /Users/siwei/repos/agent_self/percentage.py --lean={lean} --temperature={temperature} --model={model} --focus={focus} --task={task} --iteration={iteration}")
